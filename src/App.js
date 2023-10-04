8a7674c80101441825a2e4d8cbb1fbb52527febe
Saekyul Sammy Song <sammy.song259@gmail.com>
2023-10-04 06:28:47 -0500

    Fixed Wrapping Bug for API KEY

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=process.env.REACT_APP_WEATHER_API_KEY`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
